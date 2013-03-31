dd701b07dbe6ee8344c8083eca0a8b77f2264d2a
gvaneyck <gvaneyck@users.noreply.github.com>
2013-03-31 04:49:10 +0000

    Support directories being generated as classes

--- src/com/gvaneyck/ggengine/ActionTest.java
+++ src/com/gvaneyck/ggengine/ActionTest.java
@@ -1,11 +1,16 @@
 package com.gvaneyck.ggengine;
 
+import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import com.gvaneyck.util.json.JSON;
 import com.gvaneyck.util.json.ObjectMap;
@@ -15,32 +20,51 @@ public class ActionTest {
 		ActionTest at = new ActionTest();
 		at.test();
 	}
+	
+	public ArrayList<String> funcs = new ArrayList<String>();
 		
 	public void test() {
-		StringBuilder scriptText = new StringBuilder();
-		File scriptDir = new File("scripts");
-		for (File script : scriptDir.listFiles())
-			scriptText.append(parseScript(script));
+		String scriptText = loadDir(new File("scripts"), "");
 		
-		System.out.println(scriptText.toString());
+		System.out.println(scriptText);
 
-		GroovyShell shell = new GroovyShell();
-		Script executor = shell.parse(scriptText.toString());
+		Map<String, Object> gs = new HashMap<String, Object>();
+		Binding binding = new Binding();
+		binding.setVariable("gs", gs);
+		GroovyShell shell = new GroovyShell(binding);
+		Script executor = shell.parse(scriptText);
 		
 		// Test run all the functions
-		for (File script : scriptDir.listFiles()) {
-			String func = script.getName();
-			if (!func.endsWith(".act"))
-				continue;
-			func = func.substring(0, func.lastIndexOf("."));
+		for (String func : funcs) {
+			System.out.println("--------");
+			System.out.println(func + ":");
 			executor.invokeMethod(func, null);
+			System.out.println("--------");
 		}
+		
+		// Test the game state
+		System.out.println(gs);
 	}
 	
-	public String parseScript(File file) {
-		if (!file.getName().endsWith(".act"))
-			return "";
+	public String loadDir(File dir, String clazz) {
+		StringBuilder scriptText = new StringBuilder();
 		
+		for (File script : dir.listFiles()) {
+			if (script.isDirectory()) {
+				String subClazz = clazz + "_" + script.getName();
+				scriptText.append("class " + subClazz + " {\n");
+				scriptText.append(loadDir(script, subClazz));
+				scriptText.append("}\n");
+			}
+			else if (script.isFile() && script.getName().endsWith(".act")) {
+				scriptText.append(parseScript(script, !clazz.isEmpty()));				
+			}
+		}
+		
+		return scriptText.toString();
+	}
+	
+	public String parseScript(File file, boolean inClass) {
 		String func = file.getName();
 		func = func.substring(0, func.lastIndexOf("."));
 		
@@ -49,10 +73,19 @@ public class ActionTest {
 			String fileText = readFile(file);
 			ObjectMap data = (ObjectMap)JSON.parse(fileText);
 			
-			scriptText.append("def " + func + "() {\n");
-			scriptText.append(data.getString("code").trim());
-			scriptText.append("\n}\n\n");
-
+			String code = data.getString("code").trim();
+			code =  Pattern.compile("^", Pattern.MULTILINE).matcher(code).replaceAll("\t");
+			if (!inClass) {
+				scriptText.append("def " + func + "() {\n");
+				scriptText.append(code + "\n");
+				scriptText.append("}\n\n");
+				funcs.add(func);
+			}
+			else {
+				scriptText.append("\tstatic " + func + "() {\n");
+				scriptText.append("\t" + code + "\n");
+				scriptText.append("\t}\n\n");
+			}
 		}
 		catch (Exception e) {
 			System.out.println("Failed to parse script named '" + file.getName() + "'");
