541e3d898f7859de21f7741f9af29fed372bcba1
gvaneyck <gvaneyck@users.noreply.github.com>
2013-10-11 05:30:39 +0000

    First iteration of game manager

--- src/com/gvaneyck/ggengine/GameManager.java
+++ src/com/gvaneyck/ggengine/GameManager.java
@@ -1,6 +1,110 @@
 package com.gvaneyck.ggengine;
 
+import groovy.lang.GroovyClassLoader;
+import groovy.lang.GroovyObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameManager {
-    private String actionDir = "actions";
-    private String classDir = "classes";
+    private Map<String, Object> gs = new HashMap<String, Object>();
+    private Map<String, Class> clazzes = new HashMap<String, Class>();
+    private Map<String, GroovyObject> instances = new HashMap<String, GroovyObject>();
+    private GroovyClassLoader loader = null;
+    
+    private String source;
+    
+    public static void main(String[] args) {
+        GameManager gm = new GameManager("tictactoe");
+        gm.start();
+    }
+    
+    public GameManager(String source) {
+        this.source = source;
+        loadClasses();
+        injectMembers();
+    }
+    
+    public void start() {
+        instances.get("TicTacToe").invokeMethod("print", null);
+    }
+    
+    private void loadClasses() {
+        if (loader == null) {
+            ClassLoader parentLoader = this.getClass().getClassLoader();
+            loader = new GroovyClassLoader(parentLoader);
+            loader.addClasspath(source);
+        }
+        
+        File sourceDir = new File(source);
+        if (!sourceDir.exists() || !sourceDir.isDirectory()) {
+            System.err.println("Source directory does not exist: " + source);
+            return;
+        }
+        
+        File[] sourceFiles = sourceDir.listFiles();
+        for (File f : sourceFiles) {
+            String fileName = f.getName();
+            if (fileName.endsWith(".groovy")) {
+                String clazz = fileName.substring(0, fileName.length() - 7);
+                loadClass(clazz);
+            }
+        }
+    }
+    
+    private void loadClass(String clazz) {
+        try {
+            Class groovyClass = loader.loadClass(clazz);
+            GroovyObject instance = (GroovyObject)groovyClass.newInstance();
+            clazzes.put(clazz, groovyClass);
+            instances.put(clazz, instance);
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println("Found file named " + clazz + ".groovy, but it wasn't a class");
+        }
+        catch (InstantiationException e) {
+            System.err.println("Failed to instantiate " + clazz);
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            System.err.println("Illegal access exception for " + clazz);
+            e.printStackTrace();
+        }
+    }
+    
+    private void injectMembers() {
+        for (String name : instances.keySet()) {
+            GroovyObject instance = instances.get(name);
+            Class clazz = clazzes.get(name);
+            for (Method m : clazz.getMethods()) {
+                String method = m.getName();
+                if (!method.startsWith("set"))
+                    continue;
+                
+                String member = method.substring(3);
+                if (member.equals("Gs"))
+                    instance.invokeMethod("setGs", gs);
+                if (clazzes.containsKey(member))
+                    instance.invokeMethod(method, instance);
+            }
+        }
+    }
+    
+    private String readFile(String file) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
+        String line;
+        
+        StringBuilder buff = new StringBuilder();
+        while ((line = in.readLine()) != null)
+            buff.append(line + "\n");
+        
+        in.close();
+        return buff.toString();
+    }
 }
--- src/com/gvaneyck/ggengine/GameTest.java
+++ src/com/gvaneyck/ggengine/GameTest.java
@@ -3,146 +3,71 @@ package com.gvaneyck.ggengine;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyObject;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class GameTest {
     public static Map<String, Object> gs = new HashMap<String, Object>();
-    public static Map<String, Class> classMap = new HashMap<String, Class>();
-    public static Map<String, GroovyObject> objectMap = new HashMap<String, GroovyObject>();
-
+    public static Map<String, Class> clazzes = new HashMap<String, Class>();
+    public static Map<String, GroovyObject> instances = new HashMap<String, GroovyObject>();
+    public static GroovyClassLoader loader = null;
+    
     public static void main(String[] args) {
         try {
-            ResourceLoader resources = new ResourceLoader();
-            resources.loadActions("tictactoe");
-            resources.compileResources("classes2");
-
-            Map<String, Map<String, Action>> actions = resources.getActions();
-            ClassLoader parent = (new ActionTest()).getClass().getClassLoader();
-            GroovyClassLoader loader = new GroovyClassLoader(parent);
-            loader.addClasspath("classes2");
-
-            // Set up Util class
-            Class utilClass = loader.loadClass("Util");
-            GroovyObject utilObj = (GroovyObject)utilClass.newInstance();
-            utilObj.setProperty("gs", gs);
-
-            for (String clazz : actions.keySet()) {
-                Class groovyClass = loader.loadClass(clazz);
-                classMap.put(clazz, groovyClass);
-            }
-            gs.put("classes", classMap);
-
-            runGame();
+            ClassLoader parent = (new GameTest()).getClass().getClassLoader();
+            loader = new GroovyClassLoader(parent);
+            loader.addClasspath("tictactoe");
+            
+            loadClass("TicTacToe");
+            injectMembers();
+            instances.get("TicTacToe").invokeMethod("print", null);
+            
+            loader.close();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static void runGame() throws Exception {
-        Scanner in = new Scanner(System.in);
-        GroovyObject obj = (GroovyObject)classMap.get("Rules").newInstance();
-        obj.setProperty("gs", gs);
-        obj.invokeMethod("init", null);
-
-        while (true) {
-            printGameState();
-            checkEnd();
-
-            List<Choice> choices = getChoices();
-            printChoices(choices);
-
-            Choice choice = choices.get(in.nextInt());
-            invokeAction(choice.action, choice.args);
-
-            if (gs.get("activePlayer").equals("X"))
-                gs.put("activePlayer", "O");
-            else
-                gs.put("activePlayer", "X");
-        }
+    
+    public static void loadClass(String clazz) throws Exception{
+        Class groovyClass = loader.loadClass(clazz);
+        GroovyObject instance = (GroovyObject)groovyClass.newInstance();
+        clazzes.put(clazz, groovyClass);
+        instances.put(clazz, instance);
     }
-
-    public static void printChoices(List<Choice> choices) {
-        for (int i = 0; i < choices.size(); i++) {
-            Choice choice = choices.get(i);
-            System.out.print(i + ") " + choice.action);
-            if (choice.args != null) {
-                System.out.print(", " + choice.args);
+    
+    public static void injectMembers() {
+        for (String name : instances.keySet()) {
+            GroovyObject instance = instances.get(name);
+            Class clazz = clazzes.get(name);
+            for (Method m : clazz.getMethods()) {
+                String method = m.getName();
+                if (!method.startsWith("set"))
+                    continue;
+                
+                String member = method.substring(3);
+                if (member.equals("Gs"))
+                    instance.invokeMethod("setGs", gs);
+                if (clazzes.containsKey(member))
+                    instance.invokeMethod(method, instance);
             }
-            System.out.println();
         }
     }
-
-    public static List<Choice> getChoices() throws InstantiationException, IllegalAccessException {
-        List<Choice> choices = new ArrayList<Choice>();
-
-        GroovyObject obj = getObject("Rules");
-        Object result = obj.invokeMethod("getActions", null);
-
-        if (result instanceof List) {
-            List<?> resultList = (List<?>)result;
-            for (Object item : resultList)
-                choices.add(Choice.getChoice(item));
-        }
-        else {
-            choices.add(Choice.getChoice(result));
-        }
-
-        return choices;
-    }
-
-    public static Object invokeAction(String action) throws InstantiationException, IllegalAccessException {
-        return invokeAction(action, null);
-    }
-
-    public static Object invokeAction(String action, Object args) throws InstantiationException, IllegalAccessException {
-        int idx = action.indexOf('.');
-        String clazzName = action.substring(0, idx);
-        String methodName = action.substring(idx + 1);
-
-        GroovyObject obj = getObject(clazzName);
-        return obj.invokeMethod(methodName, args);
-    }
-
-    public static GroovyObject getObject(String clazzName) throws InstantiationException, IllegalAccessException {
-        if (!objectMap.containsKey(clazzName)) {
-            GroovyObject obj = (GroovyObject)classMap.get(clazzName).newInstance();
-            obj.setProperty("gs", gs);
-            objectMap.put(clazzName, obj);
-        }
-
-        return objectMap.get(clazzName);
-    }
-
-    public static void printGameState() {
-        System.out.println("Current player: " + gs.get("activePlayer"));
-        String[][] board = (String[][])gs.get("board");
+    
+    public static String readFile(String file) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
+        String line;
+        
         StringBuilder buff = new StringBuilder();
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                if (board[x][y] != null)
-                    buff.append(board[x][y]);
-                else
-                    buff.append(' ');
-                if (y < 2)
-                    buff.append('|');
-            }
-            buff.append('\n');
-            if (x < 2)
-                buff.append("-+-+-\n");
-        }
-        System.out.println(buff.toString());
-    }
-
-    public static void checkEnd() throws Exception {
-        boolean result = Boolean.valueOf((Boolean)invokeAction("Rules.checkEnd", null));
-        if (result) {
-            System.out.println("We have a winner!");
-            System.exit(0);
-        }
+        while ((line = in.readLine()) != null)
+            buff.append(line + "\n");
+        
+        in.close();
+        return buff.toString();
     }
 }
