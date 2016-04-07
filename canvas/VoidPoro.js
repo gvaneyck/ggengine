79f144a831489949df861bf5e86001008d319afa
Gabriel Van Eyck <vaneyckster@gmail.com>
2016-04-06 20:18:06 -0700

    Icon test

--- canvas/VoidPoro.js
+++ canvas/VoidPoro.js
@@ -11,4 +11,17 @@ function loadGameState() {
 
 function handleActions() {
     console.log(state.actions);
+
+    var label = new Label(10, 10, 'Select your champion');
+    uiManager.addElement(label);
+
+    var xPos = 10;
+    for (var idx in state.actions) {
+        var action = state.actions[idx];
+        var image = new Picture(xPos, 33, 100, 100, 'icons/' + action.args[1] + '.png');
+        xPos += 110;
+        uiManager.addElement(image);
+    }
+
+    uiManager.dirty = true;
 }
